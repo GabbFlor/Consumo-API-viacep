417e619cd3d3d5b4f65280c24344153c0cc7cfc7
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-11-06 17:28:55 -0300

    feat
    - HTML finalizado;
    - Metade do CSS pronto;

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <main>
+      <form className='form-entrada'>
+        <h1>Consulta de CEP</h1>
+
+        <div>
+          <input type="text" placeholder='Informe o seu CEP...' />
+
+          <button type='submit' title='Buscar...'>
+            <i class="bi bi-search"/>
+          </button>
+        </div>
+      </form>
+
+      <form className='form-retorno'>
+        <div>
+          <p>Rua</p>
+          <input type="text"/>
+        </div>
+
+        <div>
+          <p>Bairro</p>
+          <input type="text"/>
+        </div>
+
+        <div>
+          <p>Cidade</p>
+          <input type="text"/>
+        </div>
+
+        <div>
+          <p>Estado</p>
+          <input type="text"/>
+        </div>
+      </form>
+    </main>
   )
 }
 
