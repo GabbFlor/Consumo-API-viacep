bdbacded72fcbdf29b925f139bfc0548c88c1bdc
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-11-07 14:58:30 -0300

    feat
    - Bgl pronto para começar a fazer as requisições

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,72 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [ cep, setCep ] = useState("");
+  const [ rua, setRua ] = useState("");
+  const [ bairro, setBairro ] = useState("");
+  const [ cidade, setCidade ] = useState("");
+  const [ estado, setEstado ] = useState("");
+  const [ inputsDesativados, setInputsDesativados ] = useState(true);
+
   return (
     <main>
       <form className='form-entrada'>
         <h1>Consulta de CEP</h1>
 
         <div>
-          <input type="text" placeholder='Informe o seu CEP...' />
+          <input 
+            type="text" 
+            placeholder='Informe o seu CEP...' 
+            value={cep}
+            onChange={(e) => setCep(e.target.value)}
+          />
 
           <button type='submit' title='Buscar...'>
-            <i class="bi bi-search"/>
+            <i className="bi bi-search"/>
           </button>
         </div>
       </form>
 
       <form className='form-retorno'>
         <div>
-          <p>Rua</p>
-          <input type="text"/>
+          <p>Rua:</p>
+          <input 
+            type="text" 
+            disabled={inputsDesativados}
+            value={rua}
+            onChange={(e) => setRua(e.target.value)}
+          />
         </div>
 
         <div>
-          <p>Bairro</p>
-          <input type="text"/>
+          <p>Bairro:</p>
+          <input 
+            type="text" 
+            disabled={inputsDesativados}
+            value={bairro}
+            onChange={(e) => setBairro(e.target.value)}
+          />
         </div>
 
         <div>
-          <p>Cidade</p>
-          <input type="text"/>
+          <p>Cidade:</p>
+          <input 
+            type="text" 
+            disabled={inputsDesativados}
+            value={cidade}
+            onChange={(e) => setCidade(e.target.value)}
+          />
         </div>
 
         <div>
-          <p>Estado</p>
-          <input type="text"/>
+          <p>Estado:</p>
+          <input 
+            type="text" 
+            disabled={inputsDesativados}
+            value={estado}
+            onChange={(e) => setEstado(e.target.value)}
+          />
         </div>
       </form>
     </main>
